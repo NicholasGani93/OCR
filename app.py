363bed9d9191ceff88b10118f7954a4de1fefeaa
nicholasgani <nicholasgani93@gmail.com>
2023-06-09 19:24:41 +0700

    Make directory for temp image

--- app.py
+++ app.py
@@ -43,6 +43,8 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+os.mkdir(app.config['UPLOAD_FOLDER'])
+
 @app.route('/')
 def index():
     return jsonify({'message': 'Welcome'})
