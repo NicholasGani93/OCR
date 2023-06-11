2799a7c9bdd523882b636e5b2d5e9497f29ba799
nicholasgani <nicholasgani93@gmail.com>
2023-06-11 13:02:39 +0700

    Accept image from url

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, redirect, jsonify
 from werkzeug.utils import secure_filename
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest,HTTPException
 import urllib.request
 from PIL import Image, ImageDraw, ImageFont
 import base64
@@ -65,30 +65,34 @@ def upload_file():
 		resp.status_code = 400
 		return resp
 	# check if the post request has the file part
-	if 'file' not in request.files or 'image_url' not in request.form:
+	if 'file' not in request.files and 'image_url' not in request.form:
 		resp = jsonify(ResponseBody('No file part in the request').__dict__)
 		resp.status_code = 400
 		return resp
-	file = request.files['file']
-	image_url = request.form['image_url']
-	if file.filename == '' or image_url == '':
+	file = None
+	if 'file' in request.files:
+		file = request.files['file']
+	image_url = ''
+	if 'image_url' in request.form:
+		image_url = request.form['image_url']
+	if (file == None or file.filename == '') and image_url == '':
 		resp = jsonify(ResponseBody('No file selected for uploading').__dict__)
 		resp.status_code = 400
 		return resp
 	result_image = None
-	if file and allowed_file(file.filename):
+	if image_url != '':
+		filename = 'temp.jpg'
+		urllib.request.urlretrieve(image_url,filename)
+		result_image = watermark(filename,watermark_text)
+	elif file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		fullpath = os.path.join(os.getcwd(),app.config['UPLOAD_FOLDER'],filename)
 		file.save(fullpath)
 		result_image = watermark(fullpath,watermark_text)
-	else :
-		filename = 'temp.jpg'
-		urllib.request.urlretrieve(image_url,filename)
-		result_image = watermark(filename,watermark_text)
-	# else:
-	# 	resp = jsonify(ResponseBody('Allowed file types are txt, pdf, png, jpg, jpeg, gif').__dict__)
-	# 	resp.status_code = 400
-	# 	return resp
+	else:
+		resp = jsonify(ResponseBody('Allowed file types are png, jpg, jpeg').__dict__)
+		resp.status_code = 400
+		return resp
 	resp_body = ResponseBody('Success apply watermark')
 	resp_body.image_result = result_image.decode('utf-8')
 	resp = jsonify(resp_body.__dict__)
@@ -98,9 +102,21 @@ def upload_file():
 @app.errorhandler(BadRequest)
 def handle_bad_request(e):
     return 'bad request!', 400
+    
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+	"""Return JSON instead of HTML for HTTP errors."""
+	# start with the correct headers and status code from the error
+	response = e.get_response()
+	# replace the body with JSON
+	resp_body = jsonify(ResponseBody('Internal Server error').__dict__)
+	resp_body.status_code = 500
+	response.data = resp_body
+	response.content_type = "application/json"
+	return response
 
 # or, without the decorator
 app.register_error_handler(400, handle_bad_request)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
